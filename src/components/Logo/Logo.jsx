5ebde4753d723757f4b960037d61a8e57090616b
Naganandhini <naganandhiniak2002@gmail.com>
2024-06-18 09:46:04 +0530

    Module 1

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import logo from "../../assets/logoImage.png";
 
 const Logo = () => {
-  return <img src={logo} alt="logo" width={67} />;
+  return(
+    <div className="logo-container">
+     <img src={logo} alt="Qtify Logo" className="logo" width={67} />
+     </div>
+  );
 };
 
 export default Logo;
